b9b2c502fbca158310456cbd55dc10193a218765
miraclemarf <marfy.naked@gmail.com>
2019-05-12 02:49:08 +0700

    css config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,18 @@ const Dotenv = require('dotenv-webpack')
 const withSass = require('@zeit/next-sass')
 const withCss = require('@zeit/next-css')
 module.exports = 
-withCss,
+withCss({
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          name: '[name].[ext]'
+        }
+      }
+    }),
 withSass(
     {
       

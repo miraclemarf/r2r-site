b153c234b10fa5f9514de7e538cae78ae1a69ae4
miraclemarf <marfy.naked@gmail.com>
2019-06-24 16:35:31 +0700

    cateogry name for helm

--- utils/accessories.js
+++ utils/accessories.js
@@ -1,7 +1,7 @@
 import fetch from 'isomorphic-unfetch';
 
 export const getHelmList= async () =>{
-    const result = await fetch(process.env.API_URL+'/accessories/helm')
+    const result = await fetch(process.env.API_URL+'/accessories/riding%20pants')
     const data = await result.json()
     
     return data;
